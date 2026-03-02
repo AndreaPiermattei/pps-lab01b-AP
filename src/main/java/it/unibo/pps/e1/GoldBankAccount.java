70da912261ffc99cabf0f2adb0ce2e92de3bf266
Andrea Piermattei - andrea.piermattei@studio.unibo.it <andrea.piermattei@studio.unibo.it>
2026-03-02 09:54:22 +0100

    stating working on goldAccount

--- src/main/java/it/unibo/pps/e1/GoldBankAccount.java
+++ src/main/java/it/unibo/pps/e1/GoldBankAccount.java
@@ -1,4 +1,29 @@
 package it.unibo.pps.e1;
 
-public class GoldBankAccount {
+public class GoldBankAccount implements BankAccount{
+
+    private CoreBankAccount base;
+
+    public GoldBankAccount(CoreBankAccount base) {
+        this.base = base;
+    }
+
+    @Override
+    public int getBalance() {
+        return this.base.getBalance();
+    }
+
+    @Override
+    public void deposit(int amount) {
+        base.deposit(amount);
+    }
+
+    @Override
+    public void withdraw(int amount) {
+        int overdraftLimit = -500;
+        if ((this.getBalance()-amount)>overdraftLimit){
+            throw new IllegalStateException();
+        }
+        base.withdraw(amount);
+    }
 }
